3fff19053f059c28f8bbb7ca9e9253f44cf29129
Alcenir Felix <afelix@softquim.com.br>
2021-02-27 01:40:18 +0000

    Aula ovly

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -10,9 +10,11 @@ sap.ui.define([
 		return Controller.extend("ovly.odatawrite.controller.Main", {
 
             oList: null,
+            oPageDetail: null,
 
 			onInit: function () {
                 this.oList = this.byId("list");
+                this.oPageDetail = this.byId("page-detail");
             },
 
 
@@ -20,6 +22,10 @@ sap.ui.define([
                 var oSource = oEvent.getSource();
                 var oParameters = oEvent.getParameters();
                 var oListItem = oParameters.listItem;
+                var oContext = oListItem.getBindingContext();
+
+                this.byId("header").setBindingContext(oContext);
+                this.oPageDetail.setBindingContext(oContext);
             },
 
             onLiveChange: function(oEvent){
@@ -39,6 +45,37 @@ sap.ui.define([
                 var oBinding = this.oList.getBinding("items"); // sap.ui.model.odata.v2.ODataListBinding
 
                 oBinding.filter(aFilter);
+            },
+
+            onSubmitReview: function(oEvent) {
+                var oDataModel = this.getOwnerComponent().getModel();
+                // window.oDataModel = oDataModel;
+
+                var oContext = this.oPageDetail.getBindingContext();
+                var sProductId = oContext.getObject().Id;
+                // var sProductId = oContext.getProperty("Id");
+                var oReview = {};
+                oReview.ProductId = sProductId;
+                oReview.Rating = this.byId("rating").getValue();
+                oReview.Comment = this.byId("comment").getValue();
+
+                oEvent.getSource().setBusy(true);
+
+                var that = this;
+                // var oButton = oEvent.getSource();
+                var mParameters = {
+                    success: function (oData, response) {
+                        sap.m.MessageToast.show("sucesso");
+                        this.setBusy(false);
+                    }.bind(oEvent.getSource()),
+                    error: function (oData, response) {
+                        sap.m.MessageToast.show("error");
+                        that.getSource().setBusy(false);
+                        // oButton.setBusy(false);
+                    }
+                };
+                oDataModel.create("/Reviews", oReview, mParameters)
+
             }
 
 		});
